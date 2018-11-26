7cc571c1302517917ca8e79b9c11db057e96bfc2
mountainflo <tobusch@hm.edu>
2018-11-26 21:14:19 +0100

    publishing cones relative to the car

--- src/cone_publisher.cpp
+++ src/cone_publisher.cpp
@@ -1,67 +1,77 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 #include <visualization_msgs/Marker.h>
+#include <sstream>
+
+
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "cone_publisher");
 
   ros::NodeHandle n;
   ros::Publisher cone_pub = n.advertise<visualization_msgs::Marker>("cone_publisher", 50);
-  tf::TransformBroadcaster odom_broadcaster;
+  tf::TransformListener listener;
 
+  double x_left_cone = 5.0;
+  double y_left_cone = -5.0;
+  double x_right_cone = 2.0;
+  double y_rigth_cone = 2.0;
 
-  double x = -1.0;
-  double y = 0.0;
-  double th = 0.0;
-
-  double vx = 5.75;
-  double vy = -5.75;
-  double vth = 1;
-
-  ros::Time current_time, last_time;
+  ros::Time current_time;
   current_time = ros::Time::now();
-  last_time = ros::Time::now();
 
-  int max_amount_cones = 50;
+  int max_amount_cones = 20;
 
-  ros::Rate r(10.0);
+  ros::Rate rate(1.0);
   int count = 0;
-  while(n.ok()){
+  while (n.ok()){
 
-    
     if (count > max_amount_cones) {
       count = 0;
     }
 
     ros::spinOnce(); // check for incoming messages
+
+    /*tf::StampedTransform transform;
+    try{
+
+      //listener.waitForTransform("/base_link", "/odom", ros::Time(0), ros::Duration(10.0) );
+
+      listener.lookupTransform("base_link", "base_link",  
+                                ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+    }*/
+
+    //------------------------------------------------------------
+    //publish left cone
+    
     current_time = ros::Time::now();
 
-
-    //------------------------------------------------------------
-    //compute odometry in a typical way given the velocities of the robot
-    double dt = (current_time - last_time).toSec();
-    double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-    double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-    double delta_th = vth * dt;
-
-    x += delta_x;
-    y += delta_y;
-    th += delta_th;
-
-
-    //------------------------------------------------------------
-    //next, we'll publish the odometry message over ROS
     visualization_msgs::Marker marker;
-    marker.header.frame_id = "odom";
-    marker.header.stamp = current_time; // 0 -> always displayed marker
+    marker.header.frame_id = "base_link";
+    marker.header.stamp = current_time;
     marker.id = count;
 
     marker.type = visualization_msgs::Marker::SPHERE;
     marker.action = visualization_msgs::Marker::ADD;
 
-    marker.pose.position.x = x; //move x position over time
-    marker.pose.position.y = y;
+    //ROS_INFO("transform.getOrigin().x() = %f", transform.getOrigin().x());
+    //ROS_INFO("transform.getOrigin().y() = %f", transform.getOrigin().y());
+
+
+
+    marker.pose.position.x = x_left_cone; 
+    marker.pose.position.y = y_left_cone;
     marker.pose.position.z = 0.0;
+
+    /*marker.pose.orientation.x = transform.getRotation().x();
+    marker.pose.orientation.y = transform.getRotation().y();
+    marker.pose.orientation.z = transform.getRotation().z();
+    marker.pose.orientation.w = 1.0;*/
 
     marker.scale.x = 0.5;
     marker.scale.y = 0.5;
@@ -78,8 +88,8 @@ int main(int argc, char** argv){
 
 
     //------------------------------------------------------------
-    last_time = current_time;
+    //last_time = current_time;
     ++count;
-    r.sleep();
+    rate.sleep();
   }
 }
--- src/odometry_publisher.cpp
+++ src/odometry_publisher.cpp
@@ -8,22 +8,22 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "odometry_publisher");
 
   ros::NodeHandle n;
-  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odometry_publisher", 50);
   tf::TransformBroadcaster odom_broadcaster;
 
   double x = 0.0;
   double y = 0.0;
   double th = 0.0;
 
-  double vx = 0.3;
-  double vy = -0.3;
-  double vth = 0.05;
+  double vx = 1.7;
+  double vy = -1.7;
+  double vth = 0.25;
 
   ros::Time current_time, last_time;
   current_time = ros::Time::now();
   last_time = ros::Time::now();
 
-  ros::Rate r(10.0);
+  ros::Rate r(20.0);
   while(n.ok()){
 
     ros::spinOnce();               // check for incoming messages
@@ -50,7 +50,7 @@ int main(int argc, char** argv){
     //first, we'll publish the transform over tf
     geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = current_time;
-    odom_trans.header.frame_id = "odom";
+    odom_trans.header.frame_id = "world";
     odom_trans.child_frame_id = "base_link";
 
     odom_trans.transform.translation.x = x;
@@ -66,7 +66,7 @@ int main(int argc, char** argv){
     //which will be displayed as arrow
     nav_msgs::Odometry odom;
     odom.header.stamp = current_time;
-    odom.header.frame_id = "odom";
+    odom.header.frame_id = "world";
 
     //set the position
     odom.pose.pose.position.x = x;
